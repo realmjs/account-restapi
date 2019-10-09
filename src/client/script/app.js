059af013867c01c69e4f41904f94e85197dd3bb4
duongtdn <duongtdn@gmail.com>
2019-10-09 08:21:23 +0000

    communicate with account-client via postMessage

--- src/client/script/app.js
+++ src/client/script/app.js
@@ -5,6 +5,18 @@ if (module.hot) { module.hot.accept() }
 import React from 'react'
 import { render } from "react-dom"
 
+import { postMessage } from "../../lib/message"
 import App from '../Template/App'
 
-render(<App data = {__data} />, document.getElementById("root"))
+document.addEventListener("DOMContentLoaded", function(event) {
+  postMessage('iframe.loaded',  {height: 615, width: 460})
+  render(<App data = {__data} xclose = {xclose} xdone = {xdone} />, document.getElementById("root"))
+})
+
+function xclose() {
+  postMessage('iframe.close')
+}
+
+function xdone(attr) {
+  postMessage('iframe.done', attr)
+}
