7bbfe8f14aafd6e8edcb145f54619fde072b3c88
duongtdn <duongtdn@gmail.com>
2019-10-17 00:45:03 +0700

    SSO support query ?return=json

--- src/api/session/sso.js
+++ src/api/session/sso.js
@@ -7,9 +7,13 @@ function validateParameters(helpers) {
   return function(req, res, next) {
     if (!(req.query && req.query.app)) {
       helpers.alert &&  helpers.alert('Bad Request: Missing app in query')
-      const data = { route: 'error', error: {code: 400, detail: 'Bad request'} }
-      res.writeHead( 400, { "Content-Type": "text/html" } )
-      res.end(html({title: 'Error', data, script: process.env.SCRIPT, style: false}))
+      if (req.query && req.query.return && req.query.return === 'json') {
+        res.status(400).json({ error: 'Bad request' })
+      } else {
+        const data = { route: 'error', error: {code: 400, detail: 'Bad request'} }
+        res.writeHead( 200, { "Content-Type": "text/html" } )
+        res.end(html({title: 'Error', data, script: process.env.SCRIPT, style: false}))
+      }
       return
     }
     const app = helpers.Apps.find( app => app.id === req.query.app )
@@ -17,9 +21,13 @@ function validateParameters(helpers) {
       req.app = app
       next()
     } else {
-      const data = { route: 'error', error: {code: 404, detail: 'App not found'} }
-      res.writeHead( 200, { "Content-Type": "text/html" } )
-      res.end(html({title: 'Error', data, script: process.env.SCRIPT, style: false}))
+      if (req.query && req.query.return && req.query.return === 'json') {
+        res.status(404).json({ error: 'App not found' })
+      } else {
+        const data = { route: 'error', error: {code: 404, detail: 'App not found'} }
+        res.writeHead( 200, { "Content-Type": "text/html" } )
+        res.end(html({title: 'Error', data, script: process.env.SCRIPT, style: false}))
+      }
     }
   }
 }
@@ -32,12 +40,21 @@ function getSession() {
     .then( session => { req.uid = session.uid; next(); })
     .catch( err => {
       if (err === 'no_cookie') {
-        const data = { route: 'sso', targetOrigin: app.url, status: 200, session: null }
-        res.end(html({title: 'SSO', data, script: process.env.SCRIPT, style: false}))
+        if (req.query && req.query.return && req.query.return === 'json') {
+          res.status(200).json({session: null})
+        } else {
+          const data = { route: 'sso', targetOrigin: app.url, status: 200, session: null }
+          res.writeHead( 200, { "Content-Type": "text/html" } )
+          res.end(html({title: 'SSO', data, script: process.env.SCRIPT, style: false}))
+        }
       } else {
-        const data = { route: 'error', targetOrigin: app.url, error: {code: 400, detail: 'Bad request'} }
-        res.writeHead( 400, { "Content-Type": "text/html" } )
-        res.end(html({title: 'Error', data, script: process.env.SCRIPT, style: false}))
+        if (req.query && req.query.return && req.query.return === 'json') {
+          res.status(400).json({ error: 'Bad request' })
+        } else {
+          const data = { route: 'error', targetOrigin: app.url, error: {code: 400, detail: 'Bad request'} }
+          res.writeHead( 200, { "Content-Type": "text/html" } )
+          res.end(html({title: 'Error', data, script: process.env.SCRIPT, style: false}))
+        }
       }
     })
   }
@@ -52,20 +69,38 @@ function findUser(helpers) {
         req.user = users[0]
         next()
       } else {
-        const data = { route: 'error', targetOrigin: app.url, error: {code: 404, detail: 'no user'} }
+        if (req.query && req.query.return && req.query.return === 'json') {
+          res.status(404).json({ error: 'No user' })
+        } else {
+          const data = { route: 'error', targetOrigin: app.url, error: {code: 404, detail: 'No user'} }
+          res.writeHead( 200, { "Content-Type": "text/html" } )
+          res.end(html({title: 'Error', data, script: process.env.SCRIPT, style: false}))
+        }
+      }
+    })
+    .catch( err => {
+      helpers.alert && helpers.alert(err)
+      if (req.query && req.query.return && req.query.return === 'json') {
+        res.status(403).json({ error: 'Unable to access Database' })
+      } else {
+        const data = { route: 'error', targetOrigin: app.url, error: {code: 403, detail: 'Unable to access Database'} }
         res.writeHead( 200, { "Content-Type": "text/html" } )
         res.end(html({title: 'Error', data, script: process.env.SCRIPT, style: false}))
       }
     })
-    .catch( err => res.status(403).json({ error: '[1] Unable to access Database' }))
   }
 }
 
 function responseSuccess() {
   return function(req, res) {
-    const app = req.app
-    const data = { route: 'sso', targetOrigin: app.url, status: 200, session: { user: serializeUser(req.user), token: req.authenToken } }
-    res.end(html({title: 'SSO', data, script: process.env.SCRIPT, style: false}))
+    if (req.query && req.query.return && req.query.return === 'json') {
+      res.status(200).json({session: { user: serializeUser(req.user), token: req.authenToken }})
+    } else {
+      const app = req.app
+      const data = { route: 'sso', targetOrigin: app.url, status: 200, session: { user: serializeUser(req.user), token: req.authenToken } }
+      res.writeHead( 200, { "Content-Type": "text/html" } )
+      res.end(html({title: 'SSO', data, script: process.env.SCRIPT, style: false}))
+    }
   }
 }
 
