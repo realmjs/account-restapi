d9b2cc75440d1c466fda83256a432cf8bd8f513b
duongtdn <duongtdn@gmail.com>
2019-10-22 08:47:59 +0000

    designing Profile tab

--- src/client/Template/MyAccount.js
+++ src/client/Template/MyAccount.js
@@ -120,11 +120,231 @@ class TabPassword extends PureComponent {
 class TabProfile extends PureComponent {
   constructor(props) {
     super(props)
+    const profile = this.props.user && this.props.user.profile || undefined
+    this.state = {
+      error: {},
+      syncing: false,
+      ...this.getProfile()
+    }
+    this.resetState = this.resetState.bind(this)
   }
   render() {
     return (
-      <div>Profile</div>
+      <div>
+        {/* Full Name */}
+        <p >
+          <span >
+            <label>Full Name</label>
+            <label className="w3-right w3-text-red"> {this.state.error.fullName} </label>
+            <input className={`w3-input w3-border ${this.state.error.fullName? 'w3-border-red': ''}`}
+                    type="text"
+                    value={this.state.fullName}
+                    onChange={this.getTyped('fullName')}
+            />
+          </span>
+        </p>
+        {/* Display Name */}
+        <p>
+          <span className="w3-mobile" style={{width: '100%', display: 'inline-block'}}>
+            <label>Display Name</label>
+            <label className="w3-right w3-text-red"> {this.state.error.displayName} </label>
+            <input  className={`w3-input w3-border ${this.state.error.displayName? 'w3-border-red': ''}`}
+                    type="text"
+                    value={this.state.displayName}
+                    onChange = {this.getTyped('displayName')}
+            />
+          </span>
+        </p>
+        {/* Gender */}
+        <p>
+          <label style={{marginRight: '4px'}}> Gender: </label>
+
+          <input className="w3-radio" style={{marginRight: '4px'}} type="radio" name="gender" value="male"
+                 checked={this.state.gender === 'male'}
+                 onChange = { () => this.setState({gender: 'male'}) }
+          />
+          <label>Male</label>
+
+          <label style={{marginRight: '12px'}} />
+
+          <input className="w3-radio" style={{marginRight: '4px'}} type="radio" name="gender" value="female"
+                 checked={this.state.gender === 'female'}
+                 onChange = { () => this.setState({gender: 'female'}) }
+          />
+          <label>Female</label>
+        </p>
+        {/* Birthday */}
+        <p>
+          <span className="w3-mobile" style={{width: '35%', display: 'inline-block'}}>
+            <label>Birthday</label>
+            <input  className="w3-input w3-border"
+                    type="text"
+                    placeholder="dd/mm/yyyy"
+                    value={this.state.birthday}
+                    onChange = {e => this.getTypedBirthday(e)}
+            />
+          </span>
+        </p>
+        {/* Emails */}
+        <p>
+          <label>Email</label>
+          <label className="w3-right w3-text-red"> {this.state.error.email} </label>
+          {
+            this.state.email.map((email, index) => {
+              return (
+                <span  key = {index} style={{display: 'block', marginBottom: '4px'}}>
+                  <input  className = {`w3-input w3-border ${this.state.error.email? 'w3-border-red': ''}`}
+                          type = "text"
+                          value = {email}
+                          disabled = {index === 0}
+                          onChange = {this.getTyped('email', index)}
+                  />
+                </span>
+              )
+            })
+          }
+          <label  className = "w3-text-blue"
+                  style = {{cursor: 'pointer', display: 'block'}}
+                  onClick = {this.addMoreBox('email')} >
+            + Add more email
+          </label>
+        </p>
+        {/* Phones */}
+        <p>
+          <label>Phone</label>
+          <label className="w3-right w3-text-red"> {this.state.error.phone} </label>
+          {
+            this.state.phone.map((phone, index) => {
+              return (
+                <span  key = {index} style={{display: 'block', marginBottom: '4px'}}>
+                  <input  className = {`w3-input w3-border ${this.state.error.phone? 'w3-border-red': ''}`}
+                          type = "text"
+                          value = {phone}
+                          onChange = {this.getTyped('phone', index)}
+                  />
+                </span>
+              )
+            })
+          }
+          <label  className = "w3-text-blue"
+                  style = {{cursor: 'pointer', display: 'block'}}
+                  onClick = {this.addMoreBox('phone')} >
+            + Add more phone number
+          </label>
+        </p>
+        {/* Address */}
+        <p>
+          <label>Address</label>
+          <input  className="w3-input w3-border"
+                  type="text"
+                  value={this.state.address}
+                  onChange = {this.getTyped('address')}
+          />
+        </p>
+        <hr />
+        <p>
+          <button className="w3-button w3-blue w3-hover-blue w3-hover-opacity" onClick={this.updateProfile} disabled={this.isChanged() || this.state.syncing} > Save </button>
+          <label style={{marginRight: '8px'}} />
+          <button className="w3-button" onClick={this.resetState} disabled={this.isChanged() || this.state.syncing} > Reset </button>
+        </p>
+      </div>
     )
+  }
+  getTyped(target, index) {
+    if (index !== undefined) {
+      return (evt) => {
+        const state = {}
+        state[target] = [...this.state[target]]
+        state[target][index] = evt.target.value
+        this.setState(state)
+      }
+    } else {
+      return (evt) => {
+        const state = {}
+        state[target] = evt.target.value
+        this.setState(state)
+      }
+    }
+  }
+  getTypedBirthday(evt) {
+    const str = evt.target.value.trim().replace(/\/\/+/, '/')
+    // skip if there's any non-digit (except splash/ charactor)
+    if (/[^0-9]/g.test(str.replace(/\//g,''))) {
+      return
+    }
+    // handle backspace
+    const last = this.state.birthday.replace(/.$/, '')
+    if (str === last) {
+      if (/\d\//.test(this.state.birthday)) {
+        this.setState({ birthday: str.replace(/\d$/, '') })
+      } else {
+        this.setState({ birthday: str })
+      }
+      return
+    }
+    // auto insert /
+    if (/(^\d\d$|^\d\d\/\d\d$)/.test(str)) {
+      this.setState({ birthday: str + '/' })
+      return
+    }
+    // limit to dd/mm/yyyy
+    if (/(^\d\d\/\d\d\/\d\d\d\d.$)/.test(str)) {
+      return
+    }
+    this.setState({ birthday: str})
+  }
+  checkBirthday(bday) {
+    const dd = bday.match(/^\d\d\//)[0].replace(/\//g,'')
+    const mm = bday.match(/\/\d\d\//)[0].replace(/\//g,'')
+    const yyyy = bday.match(/\/\d\d\d\d$/)[0].replace(/\//g,'')
+    // validate year
+    if (parseInt(yyyy) < 1900 || parseInt(yyyy) > parseInt((new Date()).getFullYear())) {
+      console.error('Invalid Year!')
+      return false
+    }
+    // validate month
+    if (parseInt(mm) < 1 || parseInt(mm) > 12) {
+      console.error('Invalid Month!')
+      return false
+    }
+    // validate day
+    if (parseInt(dd) > parseInt((new Date(yyyy, mm, 0)).getDate())) {
+      console.error('Invalid Day!')
+      return false
+    }
+    console.log('Valid Date!')
+    return true
+  }
+  addMoreBox(box) {
+    return (evt) => {
+      const _state = {}
+      const _container = [...this.state[box]]
+      _container.push('');
+      _state[box] = _container;
+      this.setState({ ..._state })
+    }
+  }
+  isChanged() {
+    const profile = this.getProfile()
+    if (!profile) { return false }
+    return Object.keys(profile).every(key => this.state[key] === profile[key])
+  }
+  resetState() {
+    this.checkBirthday(this.state.birthday)
+    this.setState({ ...this.getProfile() })
+  }
+  getProfile() {
+    const profile = this.props.user && this.props.user.profile || undefined
+    return {
+      fullName: profile && profile.fullName || '',
+      displayName: profile && profile.displayName || '',
+      gender: profile && profile.gender || '',
+      email: profile && profile.email || [''],
+      address: profile && profile.address || '',
+      phone: profile && profile.phone || [''],
+      birthday: profile && profile.birthday || '',
+      picture: profile && profile.picture || ''
+    }
   }
 }
 
@@ -177,7 +397,7 @@ class Tabs extends PureComponent {
 export default class MyAccount extends Component {
   constructor(props) {
     super(props)
-    this.state = { tab: 'password', user: null, toast: undefined }
+    this.state = { tab: 'profile', user: null, toast: undefined }
     this.tabs = [
       { icon: 'fas fa-key', name: 'password', label: 'change password' },
       { icon: 'far fa-address-card', name: 'profile', label: 'profile' }
