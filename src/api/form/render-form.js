0a94887d4a1c74792befa911458d8a3f4683a9a6
duongtdn <duongtdn@gmail.com>
2021-01-19 09:30:09 +0000

    Add tests for form

--- src/api/form/render-form.js
+++ src/api/form/render-form.js
@@ -4,43 +4,47 @@ const jwt = require('jsonwebtoken');
 
 const html = require('../../lib/html');
 
-function render(helpers) {
-  return function(req, res) {
-    if (!(req.query && req.query.name)) {
-      _renderError(req, res, 400, 'Invalid parameters');
-      return
-    }
-    if (!(req.query && req.query.app)) {
-      _renderError(req, res, 400, 'Invalid parameters');
-      return
-    }
-    if (req.query && req.query.name === 'reset') { // incase form contain token sent via email
-      if (!req.query.t) { res.redirect('/error/404'); return }
-      try {
-        jwt.verify(req.query.t, process.env.EMAIL_SIGN_KEY);
-      } catch (err) { res.redirect('/error/404'); return }
-    }
-    const app = helpers.Apps.find( app => app.id === req.query.app )
-    if (app) {
-      const title = req.query.title || 'Form';
-      const name = req.query.name;
-      _renderForm(req, res, name, title, app, req.query);
+function validateParams() {
+  return function(req, res, next) {
+    if (req.query && req.query.app && req.query.name) {
+      next();
     } else {
-      _renderError(req, res, 403, 'Application is not registered');
+      res.redirect('/error/400');
     }
   }
 }
 
-function _renderError(req, res, code, detail) {
-  const errorCode = {
-    '400' : '400 Bad request',
-    '403' : '403 Forbidden'
+function verifyApp(helpers) {
+  return function(req, res, next) {
+    const app = helpers.Apps.find( app => app.id === req.query.app );
+    if (app) {
+      req.app = app;
+      next();
+    } else {
+      res.redirect('/error/400');
+    }
   }
-  const data = { route: 'error', error: {code: errorCode[code], detail} }
-  if (req.query && req.query.height) { data.height= req.query.height }
-  if (req.query && req.query.width) { data.width= req.query.width }
-  res.writeHead( code, { "Content-Type": "text/html" } );
-  res.end(html({title: 'Error', data, script: process.env.SCRIPT}));
+}
+
+function validateTokenIfRequired() {
+  return function(req, res, next) {
+    if (req.query.name === 'resetpassword' && verifyRequestToken(req) === false) {
+      res.redirect('/error/400');
+    }
+    next();
+  }
+}
+
+function responseForm() {
+  return function(req, res) {
+    _renderForm(req, res, req.query.name, req.query.title || 'Form', req.app, req.query);
+  }
+}
+
+function verifyRequestToken(req) {
+  if (!req.query.t) {  return false; }
+  try { jwt.verify(req.query.t, process.env.EMAIL_SIGN_KEY); } catch (err) {  return false; }
+  return true;
 }
 
 function _renderForm(req, res, name, title, app, query) {
@@ -51,4 +55,4 @@ function _renderForm(req, res, name, title, app, query) {
   res.end(html({title, data, script: process.env.SCRIPT}));
 }
 
-module.exports = render;
+module.exports = [validateParams, verifyApp, validateTokenIfRequired, responseForm];
---
+++ test/form/form.api.test.js
@@ -0,0 +1,79 @@
+
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
+import jwt from 'jsonwebtoken';
+
+import app from '../server/app';
+import request from 'supertest';
+import { setupEnvironmentVariables, clearEnvironmentVariables } from "../util";
+
+
+beforeEach( () => jest.clearAllMocks() );
+beforeAll( () => setupEnvironmentVariables() );
+afterAll( () => clearEnvironmentVariables() );
+
+const url = '/form';
+
+test(`[GET ${url}] should response 302 and redirect to /error/400 if missing all parameters`, async () => {
+  await request(app).get(url)
+                    .expect(302)
+                    .expect('Location', '/error/400');
+});
+
+
+test(`[GET ${url}] should response 302 and redirect to /error/400 if missing name in parameters`, async () => {
+  await request(app).get(`${url}?app=test`)
+                    .expect(302)
+                    .expect('Location', '/error/400');
+});
+
+
+test(`[GET ${url}] should response 302 and redirect to /error/400 if missing app in parameters`, async () => {
+  await request(app).get(`${url}?name=signin`)
+                    .expect(302)
+                    .expect('Location', '/error/400');
+});
+
+
+test(`[GET ${url}] should response 302 and redirect to /error/400 for invalid app`, async () => {
+  await request(app).get(`${url}?app=true&name=signin`)
+                    .expect(302)
+                    .expect('Location', '/error/400');
+});
+
+
+test(`[GET ${url}] should response 302 and redirect to /error/400 if request resetpassword form without token`, async () => {
+  await request(app).get(`${url}?app=test&name=resetpassword`)
+                    .expect(302)
+                    .expect('Location', '/error/400');
+});
+
+
+test(`[GET ${url}] should response 302 and redirect to /error/400 if request resetpassword form with invalid token`, async () => {
+  await request(app).get(`${url}?app=test&name=resetpassword&t=invalid`)
+                    .expect(302)
+                    .expect('Location', '/error/400');
+});
+
+
+test(`[GET ${url}] should response 200 and return resetpassword page if request resetpassword form with valid token`, async () => {
+  const token = jwt.sign({uid:'tester'}, process.env.EMAIL_SIGN_KEY);
+  await request(app).get(`${url}?app=test&name=resetpassword&t=${token}`)
+                    .expect(200)
+                    .expect('Content-Type', /text\/html/)
+                    .then(res => {
+                      expect(res.text).toMatch(/ __data={\"route\":\"resetpassword\",/);
+                    });
+});
+
+
+test(`[GET ${url}] should response 200 and return signin page if request signin form`, async () => {
+  const token = jwt.sign({uid:'tester'}, process.env.EMAIL_SIGN_KEY);
+  await request(app).get(`${url}?app=test&name=signin`)
+                    .expect(200)
+                    .expect('Content-Type', /text\/html/)
+                    .then(res => {
+                      expect(res.text).toMatch(/ __data={\"route\":\"signin\",/);
+                    });
+});
