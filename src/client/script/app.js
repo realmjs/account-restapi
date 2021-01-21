55d743091441341025ab2a1e6ade472a9b1c492b
duongtdn <duongtdn@gmail.com>
2021-01-21 08:39:48 +0700

    re-render after hot reload

--- src/client/script/app.js
+++ src/client/script/app.js
@@ -1,6 +1,10 @@
 "use strict"
 
-if (module.hot) { module.hot.accept() }
+if (module.hot) {
+  module.hot.accept('../Template/App', () => {
+    render(<App data = {__data} close = {xclose} done = {xdone} />, document.getElementById("root"));
+  });
+}
 
 import React from 'react'
 import { render } from "react-dom"
@@ -11,7 +15,7 @@ import App from '../Template/App'
 document.addEventListener("DOMContentLoaded", function(event) {
   postMessage('iframe.loaded',  {height: __data.height || 615, width: __data.width || 460});
   render(<App data = {__data} close = {xclose} done = {xdone} />, document.getElementById("root"));
-})
+});
 
 function xclose() {
   postMessage('iframe.close');
