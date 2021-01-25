6ad9064010e3d55dbd5d38b2706b4cd3762c7bc1
duongtdn <duongtdn@gmail.com>
2021-01-25 22:24:24 +0700

    add test for sso react component

---
+++ test/template/sso.react.test.js
@@ -0,0 +1,19 @@
+"use strict"
+
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import SSO from '../../src/client/Template/SSO';
+
+beforeEach(() => window.__data = { session: 'user-session' });
+
+test('Should match snapshot and call done when mounted', () => {
+  const done = jest.fn(msg => msg);
+  const component = renderer.create(
+    <SSO done = {done} />
+  );
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+  expect(done).toHaveBeenCalled();
+  expect(done.mock.results[0].value).toEqual({ status: 200, session: 'user-session'});
+});
