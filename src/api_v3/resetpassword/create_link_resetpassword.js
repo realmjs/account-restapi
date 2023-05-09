c1165e1d017470212b4141f87416cc1dea6eaedc
duongtdn <duongtdn@gmail.com>
2023-05-09 13:57:07 +0700

    add create_link_resetpassword

---
+++ src/api_v3/resetpassword/create_link_resetpassword.js
@@ -0,0 +1,87 @@
+"use strict"
+
+import jwt from 'jsonwebtoken'
+
+import { isEmail } from '../../lib/form'
+import middlewareFactory from '../../lib/middleware_factory'
+import { alertCrashedEvent } from '../../lib/util'
+
+const validateRequest = () => (req, res, next) => {
+  if (req.body.email && isEmail(req.body.email) && req.body.app) {
+    next()
+  } else {
+    res.status(400).send('Bad Request')
+  }
+}
+
+const validateAppThenStoreToLocals = middlewareFactory.create(
+  'validateAppThenStoreToLocals',
+  'byRequestBody',
+  'create_link_resetpassword.js'
+)
+
+const checkEmailExistence = (helpers) => (req, res, next) => {
+  helpers.database.account.find({ email: req.body.email })
+  .then( user => {
+    if (user) {
+      res.locals.user = user
+      next()
+    } else {
+      res.status(404).send('Email is not registered')
+    }
+  })
+  .catch( err =>
+    helpers.alert && alertCrashedEvent(helpers.alert, 'create_link_resetpassword.js', 'checkEmailExistence', err)
+  )
+}
+
+const checkRealm = () => (req, res, next) => {
+  const realm = res.locals.app.realm
+  if (Object.keys(res.locals.user.realms).indexOf(realm) !== -1 &&
+      res.locals.user.realms[realm].roles &&
+      res.locals.user.realms[realm].roles.length > 0)
+  {
+    next()
+  } else {
+    res.status(403).send('Realm forbidden')
+  }
+}
+
+const createResetLink = () => (req, res, next) => {
+  res.locals.token = jwt.sign(
+    { uid: res.locals.user.uid, action: 'resetpassword' },
+    process.env.EMAIL_VALLIDATION_SIGN_KEY,
+    { expiresIn: process.env.EMAIL_EXPIRE_VALIDATION_LINK }
+  )
+  next()
+}
+
+const sendEmail = (helpers) => (req, res, next) => {
+  const email = req.body.email
+  const token = res.locals.token
+  const app = req.body.app
+  helpers.database.app.find({id: 'account'})
+  .then(account => {
+    helpers.hook.sendEmail({
+      to: { address: email, name: res.locals.user.profile.fullName },
+      template: 'reset_password',
+      data: { link: `${account.url}/form/account/newpassword?a=${app}&t=${token}` }
+    })
+    .then( () => next() )
+  })
+  .catch( err =>
+    helpers.alert && alertCrashedEvent(helpers.alert, 'create_link_resetpassword.js', 'sendEmail', err)
+  )
+}
+
+const final = () => (req, res) => res.status(200).send()
+
+module.exports = [
+  validateRequest,
+  validateAppThenStoreToLocals,
+  checkEmailExistence,
+  checkRealm,
+  createResetLink,
+  sendEmail,
+  final
+]
---
+++ test/api_v3/resetpassword/create_link_resetpassword.test.js
@@ -0,0 +1,142 @@
+'use strict'
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+import request from 'supertest'
+
+import { api, app } from '../../testutils/fakeserver'
+import { setupEnvironmentVariables, clearEnvironmentVariables } from '../../testutils/fakeenv'
+
+import funcs from '../../../src/api_v3/resetpassword/create_link_resetpassword'
+const endpoint = '/link/resetpassword'
+api.add(endpoint, { post: funcs })
+app.use('/', api.generate());
+
+const helpers = {
+  database: {
+    app: {
+      find: jest.fn()
+    },
+    account: {
+      find: jest.fn()
+    }
+  },
+  hook: {
+    sendEmail: jest.fn()
+  }
+}
+
+api.helpers(helpers)
+
+beforeEach( () => jest.clearAllMocks() )
+beforeAll( () => setupEnvironmentVariables() )
+afterAll( () => clearEnvironmentVariables() )
+
+
+test('Validate request and response 400', async () => {
+
+  await request(app).post(endpoint)
+  .expect(400)
+
+  await request(app).post(endpoint)
+  .set('Accept', 'application/json')
+  .send({ email: 'email@domain.ext' })
+  .expect(400)
+
+  await request(app).post(endpoint)
+  .set('Accept', 'application/json')
+  .send({ app: 'app' })
+  .expect(400)
+
+})
+
+
+test('Validate app and response 403', async () => {
+
+  helpers.database.app.find.mockResolvedValueOnce(undefined)
+
+  await request(app).post(endpoint)
+  .set('Accept', 'application/json')
+  .send({ app: 'app', email: 'email@domain.ext' })
+  .expect(403)
+
+  helpers.database.app.find.mockClear()
+
+})
+
+
+test('Verify email and response 404', async () => {
+
+  helpers.database.app.find.mockResolvedValueOnce({})
+  helpers.database.account.find.mockResolvedValueOnce(undefined)
+
+
+  await request(app).post(endpoint)
+  .set('Accept', 'application/json')
+  .send({ app: 'app', email: 'notexistemail@domain.ext' })
+  .expect(404)
+
+  helpers.database.app.find.mockClear()
+  helpers.database.account.find.mockClear()
+
+})
+
+
+test('Verify realms and response 403', async () => {
+
+  helpers.database.app.find.mockResolvedValueOnce({realm: 'test', key: 'appkey'})
+  helpers.database.account.find.mockResolvedValueOnce({
+    uid: 'uid',
+    realms: { other: { roles: ['member'] } }
+  })
+
+
+  await request(app).post(endpoint)
+  .set('Accept', 'application/json')
+  .send({ app: 'app', email: 'notexistemail@domain.ext' })
+  .expect(403)
+
+  helpers.database.app.find.mockClear()
+  helpers.database.account.find.mockClear()
+
+})
+
+
+import jwt from 'jsonwebtoken'
+test('Generate reset link and call send email hook', async () => {
+
+  const email = 'email@unit.test'
+
+  helpers.database.app.find.mockResolvedValue({realm: 'test', url: 'url'})
+  helpers.database.account.find.mockResolvedValue({
+    uid: 'uid',
+    profile: { fullName: 'Awesome' },
+    realms: { test: { roles: ['member'] } }
+  })
+  helpers.hook.sendEmail.mockResolvedValue(undefined)
+
+  await request(app).post(endpoint)
+  .set('Accept', 'application/json')
+  .send({ app: 'app', email })
+  .expect(200)
+
+  const token = jwt.sign(
+    { uid: 'uid', action: 'resetpassword' },
+    process.env.EMAIL_VALLIDATION_SIGN_KEY,
+    { expiresIn: process.env.EMAIL_EXPIRE_VALIDATION_LINK }
+  )
+
+  expect(helpers.hook.sendEmail).toHaveBeenCalledTimes(1)
+  expect(helpers.hook.sendEmail.mock.calls[0]).toEqual([{
+    to: { address: email, name: 'Awesome' },
+    template: 'reset_password',
+    data: { link: expect.stringMatching(`a=app&t=${token}`) },
+  }])
+
+  helpers.database.app.find.mockClear()
+  helpers.database.account.find.mockClear()
+  helpers.hook.sendEmail.mockClear()
+
+})
+
